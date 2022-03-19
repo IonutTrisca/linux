9bf4bc26b4204833a5acde425e013b9f381743c8
IonutTrisca <trisca46@gmail.com>
2022-03-19 15:24:50 +0200

    lab3: ex6: Added write functionality
    Signed-off-by: IonutTrisca <trisca46@gmail.com>

--- tools/labs/skels/device_drivers/kernel/so2_cdev.c
+++ tools/labs/skels/device_drivers/kernel/so2_cdev.c
@@ -126,6 +126,9 @@ so2_cdev_write(struct file *file,
 
 
 	/* TODO 5: copy user_buffer to data->buffer, use copy_from_user */
+	if (copy_from_user(data->buffer, user_buffer, size))
+		return -EFAULT;
+		
 	/* TODO 7: extra tasks for home */
 
 	return size;
@@ -157,6 +160,7 @@ static const struct file_operations so2_fops = {
 /* TODO 4: add read function */
 	.read = so2_cdev_read,
 /* TODO 5: add write function */
+	.write = so2_cdev_write,
 /* TODO 6: add ioctl function */
 };
 
